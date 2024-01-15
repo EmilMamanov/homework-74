f26d9d74d36121a604211001b412cd6afd16a276
Mamanov Emil <emil.mamanov97@gmail.com>
2024-01-15 18:50:06 +0600

    created router added functions to create and read messages

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import messagesRouter from './routes/messagesRouter';
+import fileDb from './fileDb';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
+app.use('/messages', messagesRouter);
 
 const run = async () => {
+    await fileDb.init();
+
     app.listen(port, () => {
         console.log(`Server started on ${port} port!`);
     });
