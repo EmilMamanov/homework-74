e3d76b97cf614f7e95248eef8f40687d5e355dad
Mamanov Emil <emil.mamanov97@gmail.com>
2024-01-15 18:46:16 +0600

    created main structure

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+
+const run = async () => {
+    app.listen(port, () => {
+        console.log(`Server started on ${port} port!`);
+    });
+};
+
+void run();
